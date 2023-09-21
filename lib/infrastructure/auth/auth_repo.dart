3db40a7306a0b6506820f4140bcc4fe66a4fc6d2
NivedGanga <nivedganga@gmail.com>
2023-09-21 09:42:43 +0530

    implementation of auth functions completed

--- lib/infrastructure/auth/auth_repo.dart
+++ lib/infrastructure/auth/auth_repo.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:elevate/domain/auth/i_auth_repo.dart';
 import 'package:elevate/domain/failure/main_failure.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthRepo implements IAuthRepo {
   @override
@@ -16,7 +17,8 @@ class AuthRepo implements IAuthRepo {
       if (userCredential.user != null) {
         return right(userCredential.user!.uid);
       } else {
-        return left(MainFailure.firebaseFailure('User registration failed.'));
+        return left(
+            const MainFailure.firebaseFailure('User registration failed.'));
       }
     } catch (e) {
       return left(MainFailure.firebaseFailure(e.toString()));
@@ -34,13 +36,36 @@ class AuthRepo implements IAuthRepo {
   }
 
   @override
-  Future<Either<MainFailure, String>> signin(String email, String password) {
-    // TODO: implement signin
-    throw UnimplementedError();
+  Future<Either<MainFailure, String>> signin(
+      String email, String password) async {
+    try {
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      if (userCredential.user != null) {
+        return right(userCredential.user!.uid);
+      } else {
+        return left(MainFailure.firebaseFailure('Sign-in failed.'));
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return left(MainFailure.firebaseFailure('User not found.'));
+      } else if (e.code == 'wrong-password') {
+        return left(MainFailure.firebaseFailure('Incorrect password.'));
+      } else {
+        return left(
+            MainFailure.firebaseFailure('Sign-in failed: ${e.message}'));
+      }
+    } catch (e) {
+      return left(MainFailure.firebaseFailure(e.toString()));
+    }
   }
 
   @override
-  Future<void> registerSharedPref(String uid) async {}
+  Future<void> registerSharedPref(String uid) async {
+    final sharedPref = SharedPreferences.getInstance();
+    await sharedPref.then((value) => value.setString('uid', uid));
+  }
 
   @override
   Future<Either<MainFailure, String>> googleSignIn() {
