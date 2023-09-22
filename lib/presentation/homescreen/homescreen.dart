18fdd8c57ac25811115d27e4026cb6620de488b2
Abhishek <ksabhishek00@gmail.com>
2023-09-22 19:05:55 +0530

    refactored homescreen

--- lib/presentation/homescreen/homescreen.dart
+++ lib/presentation/homescreen/homescreen.dart
@@ -1,10 +1,9 @@
 import 'package:elevate/presentation/homescreen/widgets/headerWidget.dart';
 import 'package:elevate/presentation/homescreen/widgets/quoteWidget.dart';
-import 'package:elevate/presentation/homescreen/widgets/restartButton_widet.dart';
+import 'package:elevate/presentation/homescreen/widgets/floatingActionButton_widget.dart';
 import 'package:elevate/presentation/homescreen/widgets/story_widget.dart';
 import 'package:elevate/presentation/shared/constants.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -12,11 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
-          label: Icon(Icons.mic),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButtonWidget(),
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
@@ -28,7 +23,6 @@ class HomeScreen extends StatelessWidget {
                 QuoteWidget(),
                 height,
                 StoryWidget(),
-                RestartButtonWidget(),
               ],
             ),
           ),
---
+++ lib/presentation/homescreen/widgets/floatingActionButton_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FloatingActionButtonWidget extends StatelessWidget {
+  const FloatingActionButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+          width: 250.w,
+        ),
+        FloatingActionButton(
+          heroTag: 'speak',
+          onPressed: () {},
+          child: Icon(Icons.mic),
+        ),
+        FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.restart_alt),
+          heroTag: 'mic',
+        ),
+      ],
+    );
+  }
+}
